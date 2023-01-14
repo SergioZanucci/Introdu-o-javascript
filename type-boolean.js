77db0a03c200f55544db5a30adaa478f4426c023
Sergio Zanucci <sergiozanu@gmail.com>
2023-01-14 14:32:15 -0300

    Exercicio

--- type-boolean.js
+++ type-boolean.js
@@ -13,6 +13,20 @@ const quartoNumero = 3;
 console.log(terceiroNumero === quartoNumero); //verdadeiro = true
 console.log(segundoNumero === quartoNumero);
 
+//Exercicio
+
 const texto1 = "Alura";
 const texto2 = "Programção";
 console.log(texto1 === texto2);
+
+const nome = "Laura ";
+const sobreNome = "Ayumi";
+console.log(nome + sobreNome); // true
+
+const idade = 2;
+const dezAnosDepois = "12";
+console.log(idade + dezAnosDepois); // concatena 212
+
+const number = 1;
+const text3 = "bacharel";
+console.log(number === text3); // false
