ce314a48cbfedbeb8d9e1f48ddd1ee73e6aecfa5
Sergio Zanucci <sergiozanu@gmail.com>
2023-01-18 20:46:51 -0300

    Aula 05, operadores e funções.

---
+++ expressoes.js
@@ -0,0 +1,21 @@
+function minhaFuncao (param) {
+    //bloco de codigo
+}
+
+// minhaFuncao("param")
+
+// expressao de funcao
+
+/// const soma = function(num1, num2) {return num1 + num2}
+// console.log(soma(1, 1));
+
+// diferença principal ?????
+
+console.log(apresentar());
+
+function apresentar() {
+    return "Olá";
+}
+
+console.log(soma(1, 1));
+const soma = function(num1, num2) {return num1 + num2}
