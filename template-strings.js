5eb1ab2fa807df776040d83fac5209b911b137f7
Sergio Zanucci <sergiozanu@gmail.com>
2023-01-18 19:12:08 -0300

    Aula 04, comparações.

---
+++ template-strings.js
@@ -0,0 +1,16 @@
+
+const apresentar = "Meu nome é ";
+const nome = "Sérgio ";
+const idade = 2023-1983;
+const cidadeDeNascimento = " Araraquara SP";
+
+
+console.log(apresentar + nome + idade + cidadeDeNascimento);
+
+const apresentacao = `Meu nome é $[nome], minha idade é
+${idade} e nasci na cidade de ${cidadeDeNascimento}`
+
+console.log(apresentacao);
+
+
+
